cd9e35ef4de060f8efaf675faa8cc9b41d2c4156
zhuzhenhao <zhuzhenhao@bilibili.com>
2019-06-20 17:33:04 +0800

    fix 注解，之前的写法容易踩坑

--- src/main/java/com/jason/MainApplication.java
+++ src/main/java/com/jason/MainApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,11 +18,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * @version 1.0.0
  * @date 2019/6/14 11:18
  */
-@SpringBootConfiguration
-@ComponentScan
 @Slf4j
+@SpringBootApplication
 @EnableJpaRepositories("com.jason.jpa.service")
-@EnableAutoConfiguration
 public class MainApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
